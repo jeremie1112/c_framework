44c783f6e8e3e51aee40397b6ff8998bbda054df
Paul Ambrose <pablo_rose34@hotmail.com>
2019-09-26 12:29:21 +0100

    daily upload

---
+++ ex10_v2.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	if (argc != 2) {
+	   printf("Error - requires one argument");
+	   return 1;
+	}
+
+	for(int i = 0; argv[1][i] != '\0'; i++){
+
+		if ()
+		//if number is less than 97 then add 32
+		//check if argv[1][i] is capital, if it is convert
+		//do switch statements for vowels
+	} 
+
+   return 0;
+}
